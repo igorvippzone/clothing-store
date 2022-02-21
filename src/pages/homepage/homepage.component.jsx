a2dcad72c63fda90df82e647a8d77ac6a50ad0cd
Агаларов Игорь <igorvippzone@gmail.com>
2022-02-21 06:44:00 +0300

    Добавил Header компонент и ссылки HomePge, ShopPage и Contact

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
-import { Link } from "react-router-dom";
 
 import "./homepage.style.scss";
 
@@ -8,7 +7,6 @@ const HomePage = () => {
 
 return (
   <div className="homepage">
-  <Link to="/shop">Shop</Link>
     <Directory />
   </div>
 )
